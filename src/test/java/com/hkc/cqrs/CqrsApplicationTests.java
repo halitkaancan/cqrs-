45d202967c7d0b9144c8067515ad0d1d4c21edb6
Kaan <halitkaancan@gmail.com>
2025-02-11 23:39:28 +0300

    Project started

---
+++ src/test/java/com/hkc/cqrs/CqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkc.cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
