1d1f63aeafa3c4767d141bfb0d19828a0b5fb7af
Kaan <halitkaancan@gmail.com>
2025-02-13 20:23:37 +0300

    Jwt added

---
+++ src/main/java/com/hkc/cqrs/core/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.hkc.cqrs.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+    @Configuration
+    public class SecurityConfiguration {
+
+        private final JwtFilter jwtFilter;
+
+        public SecurityConfiguration(JwtFilter jwtFilter) {
+            this.jwtFilter = jwtFilter;
+        }
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .httpBasic(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests((req) -> req
+                            .requestMatchers("/api/v1/products").authenticated()
+                            .anyRequest().permitAll()
+                    )
+                    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+            return http.build();
+        }
+    }
